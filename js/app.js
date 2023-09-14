0273bea6ab29336891a8f66b39cfcc7f927453ab
aleanrocha <ale.oficial.me@gmail.com>
2023-09-14 18:50:20 -0300

    create and add todo list

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 // Seleção de elementos
 const themes = document.querySelectorAll("#todo-theme > span")
+const todoForm = document.querySelector("#todo-form > form")
+const todoFdit = document.querySelector("#todo-edit > form")
+const todoList = document.querySelector("#todo-list")
+const todoInput = document.querySelector("#todo-input")
+const editInput = document.querySelector("#edit-input")
+const cancelEditBtn = document.querySelector("#cancel-edit-btn")
 
 // Funções
 
@@ -14,6 +20,36 @@ const changeTheme = (e) => {
   } else if (idSelected === "third-theme") {document.body.classList.add("third-gradient")}
 }
 
+// criar e adicionar as tarefas
+const saveTodo = (text) => {
+  const todoElement = document.createElement("div")
+  const todoTitle = document.createElement("h3")
+  const btnDoneTask = document.createElement("button")
+  const btnEditTask = document.createElement("button")
+  const btnDeleteTask = document.createElement("button")
+  todoTitle.innerText = text
+  todoElement.classList.add("todo")
+  btnDoneTask.classList.add("finish-todo")
+  btnEditTask.classList.add("edit-todo")
+  btnDeleteTask.classList.add("remove-todo")
+  btnDoneTask.innerHTML = `<i class="fa-solid fa-check"></i>`
+  btnEditTask.innerHTML = `<i class="fa-solid fa-pen"></i>`
+  btnDeleteTask.innerHTML = `<i class="fa-solid fa-xmark"></i>`
+  todoElement.append(todoTitle,btnDoneTask,btnEditTask,btnDeleteTask)
+  todoList.appendChild(todoElement)
+  todoInput.value = ""
+  todoInput.focus()
+}
+
 // Eventos
 
 themes.forEach((theme) => theme.addEventListener("click", changeTheme))
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const inputValue = todoInput.value
+  if(inputValue) {
+    saveTodo(inputValue)
+  } else {
+    alert("⚠️ Por favor, digite sua tarefa!")
+  }
+})
