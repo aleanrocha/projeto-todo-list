4703713f2744c06aaa3fa2713fb1938d4f85e2eb
aleanrocha <ale.oficial.me@gmail.com>
2023-09-13 23:49:51 -0300

    function changeTheme

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log("init")
+// Seleção de elementos
+const themes = document.querySelectorAll("#todo-theme > span")
+
+// Funções
+
+// trocar tema
+const changeTheme = (e) => {
+  const btnSelected = e.target
+  const idSelected = btnSelected.id
+  themes.forEach((theme) => theme.classList.remove("selected"))
+  btnSelected.classList.toggle("selected")
+  document.body.classList = ""
+  if(idSelected === "second-theme") {document.body.classList.add("second-gradient")
+  } else if (idSelected === "third-theme") {document.body.classList.add("third-gradient")}
+}
+
+// Eventos
+
+themes.forEach((theme) => theme.addEventListener("click", changeTheme))
